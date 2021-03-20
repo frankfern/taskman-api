4200c0e3550375319e5ab42b3a6f9e315df85771
Frank Fernandez <frankfernogando@gmail.com>
2021-03-19 22:12:51 -0400

    add auth controlllers

---
+++ src/auth/strategies/local.strategy.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy } from 'passport';
+
+@Injectable()
+export class LocalStrategy extends PassportStrategy(Strategy) {
+  constructor() {
+    super({
+      usernameField: 'email',
+      passwordField: 'password',
+    });
+  }
+
+  async validate(email: string, password: string) {}
+}
